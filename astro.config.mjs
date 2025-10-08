3151c898380e4129a87b6c4613de00a0fabf0c69
Nbd <122265169+10Nbd01@users.noreply.github.com>
2025-10-08 23:06:51 +0800

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,9 +27,8 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://10nbd01.github.io/",
+	site: "harmonious-lokum-01ff0c.netlify.app",
 
-	base: "/NbdWayfarer",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
